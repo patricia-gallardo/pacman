02282f7e344e48eeac5d4c3f0245fb57b2884866
Patricia Aas <psmaas@gmail.com>
2020-09-16 11:37:31 +0200

    Initial hello world

---
+++ README.md
@@ -0,0 +1,6 @@
+
+## Conan
+
+Set this environment variable and Conan will tell you which packages you need to
+intall on Linux.
+CONAN_SYSREQUIRES_MODE=verify
---
+++ conanfile.py
@@ -0,0 +1,15 @@
+from conans import ConanFile, CMake
+
+class ConanDependencies(ConanFile):
+
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+
+    def requirements(self):
+        self.requires("sdl2/2.0.12@bincrafters/stable")
+
+    def imports(self):
+        self.copy("*.dll", dst="bin", src="bin")
+        self.copy("*.dylib*", dst="bin", src="lib")
+        self.copy('*.so*', dst='lib', src='lib')
+        self.copy("license*", dst="licenses", folder=True, ignore_case=True)
